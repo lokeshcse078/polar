1a153aa2d1a4427044cafc49cbb1345e49f45664
lokeshcse078 <s.lokeeshkumar006@gmail.com>
2026-01-05 20:10:28 +0530

    Create customer.js

---
+++ static/customer.js
@@ -0,0 +1,32 @@
+function load(){
+fetch("/api/customers")
+  .then(r => r.json())
+  .then(data => {
+    const tbody = document.getElementById("customerTableBody");
+    body.innerHTML = "";
+
+tbody.innerHTML = "";
+
+if (data.length === 0) {
+    // show NOTHING when empty (as requested)
+} else {
+    data.forEach(c => {
+        tbody.innerHTML += `
+            <tr>
+                <td>${c.id}</td>
+                <td>${c.c_name}</td>
+                <td>${c.c_type}</td>
+                <td>${c.c_name}</td>
+                <td>${c.c_mail}</td>
+                <td>${c.c_phone}</td>
+                <td class="action-col">
+                    <button class="btn-edit" data-id="${c.id}">Edit</button>
+                    <button class="btn-delete" data-id="${c.id}">Delete</button>
+                </td>
+            </tr>
+        `;
+    });
+}
+})
+}
+
